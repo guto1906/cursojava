ba0dbe80d4cdafa55b175e6cfad36a97260a9dde
Augusto Soares <guto1906@gmail.com>
2018-04-22 23:52:19 -0300

    Ajuste da classe de teste

--- src/test/java/com/guto1906/cursojava/CursojavaApplicationTests.java
+++ src/test/java/com/guto1906/cursojava/CursojavaApplicationTests.java
@@ -1,4 +1,4 @@
-package com.nelioalves.cursomc;
+package com.guto1906.cursojava;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class CursomcApplicationTests {
+public class CursojavaApplicationTests {
 
 	@Test
 	public void contextLoads() {
